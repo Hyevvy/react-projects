862a06577cb475b7f67d13ee276e9b9ff6b7cddb
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-01-16 13:47:19 +0900

    20210116 성능최적화 ෆ.̮ෆ
    pureComponent와 memo를 쓰자 (ง •̀ㅁ•́)ง✧

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, {memo} from 'react';
 //가독성, 재사용성 , 성능최적화에 좋다
 //props는 부모에게 받은 것 -> 보면 부모를 찾을 것
 {/* JSX 주석은 이렇게 해용 */}
-export default class Try extends Component{
-    render(){
-        return (
-            <>
-            <li>
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
-            </li>
-            </>
-        )
-    }
-}
+
+//성능 최적화 - > pureComponent사용 (클래스일 때)
+//hooks에서는 memo로 컴퍼넌트를 감싼다
+const Try = memo(({tryInfo})=>{
+    return(
+        <li>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
+    </li>
+    )
+});
+export default Try;
